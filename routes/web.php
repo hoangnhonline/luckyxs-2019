d636721245607d24c1924fd175b9e3644696fb9e
hoang-nguyen-vn <hoangnhonline@gmail.com>
2019-02-13 15:42:52 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/test', 'TestController@index')->name('test');
-Route::get('/', function () {
-    
+Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function () {
+    Route::get('/login', ['as' => 'auth.login', 'uses' => 'LoginController@showLoginForm']);
+    Route::post('/login', ['as' => 'auth.login.post', 'uses' => 'LoginController@processLogin']);
+    Route::get('/logout', ['as' => 'auth.logout', 'middleware' => ['web'], 'uses' => 'LoginController@logout']);
+    Route::get('/forgot-password', ['as' => 'auth.forgot-password', 'uses' => 'ForgotPasswordController@forgotPass']);
+    Route::post('/forgot-password', ['as' => 'auth.forgot-password.post', 'uses' => 'ForgotPasswordController@processForgotPass']);
+});
+Route::group(['middleware' => ['auth:web', 'role']], function () {
+	Route::get('/test', 'TestController@index')->name('test');
+    Route::get('/', ['as' => 'dashboard', 'uses' => 'TestController@index']);
+    Route::get('/messages-list', ['as' => 'messages.list', 'uses' => 'TestController@messagesList']);
+    Route::get('{id}/messages-detail', ['as' => 'messages.detail', 'uses' => 'TestController@messagesDetail']);
 });
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
-
 Route::post(
     'telegram', 'TelegramController'
 );
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
